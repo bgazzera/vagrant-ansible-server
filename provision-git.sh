7f875d9e5f052c65092f892d76951014e729b2c3
Bruno Gazzera <bgazzera@paginar.com>
2015-02-13 00:27:07 +0000

    Added git credential cache
    Signed-off-by: Bruno Gazzera <bgazzera@paginar.com>

--- provision-git.sh
+++ provision-git.sh
@@ -4,4 +4,5 @@ sudo apt-get update
 sudo apt-get install -y git 
 git config --global user.email "bgazzera@paginar.com"
 git config --global user.name "Bruno Gazzera"
-
+git config --global push.default simple
+git config credential.helper 'cache --timeout=38000'
